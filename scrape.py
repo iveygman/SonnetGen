975738fca5c03119ff5fb11f82e516a56ee2bd6e
iveygman <iveygman@gmail.com>
2014-02-03 01:15:50 -0800

    Completed Shakespeare scraping

--- scrape.py
+++ scrape.py
@@ -1,6 +1,8 @@
 import urllib2
 import re
 from mechanize import Browser
+import os
+import os.path
 
 ROMAN_NUMERAL_REGEX = re.compile(r'^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$');
 
@@ -18,17 +20,28 @@ def getShakespeareSonnets():
 	endpos = localCopy.rfind(endstr)
 	# remove whitespace and keep only the interesting text
 	localCopy = [ s.lstrip().rstrip() for s in localCopy[pos:endpos].split('\r\n') if len(s)>0 ];
+
+	# create data directory
+	_path = 'data/shakespeare'
+	if not os.path.exists(_path):
+		os.makedirs(_path)
 	
-	# find indices of roman numerals
-	sonnet_starts = []
+	# make separate files
 	k = 0;
+	file = None
+	openAFile = False;
 	for line in localCopy:
 		if re.match(ROMAN_NUMERAL_REGEX,line):
-			sonnet_starts.append(k)
-		k += 1
-	
-	print localCopy
-	print sonnet_starts
+			if (file is not None):
+				file.close()
+			k += 1;
+			openAFile = True
+		else:
+			if openAFile:
+				file = open(os.path.join(_path,str(k)+'.txt'),'w');
+				openAFile = False
+			file.write(line + '\n');
+	file.close()
 
 def main():
 	getShakespeareSonnets()
